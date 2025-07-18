83f328e1dac0a64f6cd682caffa77cb8b1c1c580
Chris Pikul <chris@workmaple.com>
2025-07-18 15:07:38 +0200

    clean repo for dev version

--- llama/package.go
+++
@@ -1,5 +0,0 @@
-// Package llama uses [github.com/ebitengine/purego] to load the LLAMA LLM, which
-// in turn uses the llama-go.cpp and llama.cpp C++ libraries.
-//
-// The code in this package is copied from https://github.com/kelindar/search/blob/main
-package llama
