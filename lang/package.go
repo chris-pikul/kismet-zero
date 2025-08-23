57e92cf1db0c9cc9641066a461650921641f1cce
Chris Pikul <chris@workmaple.com>
2025-08-23 21:51:47 +0200

    transfer phoneme related stuff to own package

--- lang/package.go
+++
@@ -1,3 +0,0 @@
-// package lang is responsible for generating and managing language, lexicon,
-// linguistics, and related topics to the "written word" of a culture.
-package lang
--- lang/phoneme-pool.go
+++
@@ -1,6 +0,0 @@
-package lang
-
-type PhonemePool struct {
-	Consonants PhonemeList `json:"consonants"`
-	Vowels     PhonemeList `json:"vowels"`
-}
